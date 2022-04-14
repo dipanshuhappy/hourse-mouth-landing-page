0bf1851471f353bec980c41d0a9a79c7a5e992ce
dipanshuhappy <dipanshuhappy@gmail.com>
2022-04-14 04:52:39 -0700

    footer

--- src/screens/Footer.tsx
+++ src/screens/Footer.tsx
@@ -1,28 +1,27 @@
 import { Box, Link } from '@mui/material';
 import React from 'react';
 import { Instagram, LinkedIn, Twitter } from "@mui/icons-material";
+import styles from "../styles/Footer.module.css"
 function Footer() {
     return (
-        <Box sx={{
-            minHeight:"50vh",
-            backgroundColor:"rgb(212, 207, 255)"
-        }}>
-            <div>
+        <Box className={styles.root}>
+            <Box className={styles.links}>
                 <Link href="#" underline="none">Home</Link>
                 <Link href="#" underline="none">Become a mentor</Link>
                 <Link href="#" underline="none">Book a session</Link>
                 <Link href="#" underline="none">Terms & Conditions</Link>
-                
-                <Box display={"flex"} width={100} justifyContent={"space-between"} sx={{ ['@media (max-width:470px)']: { fontSize: 15, textAlign: "center", width: "30vw", marginTop: 2, marginBottom: 2 } }}>
-                            <Instagram color={"primary"} onClick={()=>{window.open("https://instagram.com/horsesmouth.in?utm_medium=copy_link")}} style={{cursor:"pointer"}}/>
-                            <LinkedIn color={"primary"} onClick={()=>{window.open("https://www.linkedin.com/company/horse-s-mouth")}} style={{cursor:"pointer"}}/>
-                            <Twitter color={"primary"} />
+
+                <Box display={"flex"} width={100} alignSelf="center" justifyContent="space-between" sx={{ ['@media (max-width:470px)']: { fontSize: 15, textAlign: "center", width: "30vw", marginTop: 2, marginBottom: 2 } }}>
+                    <Instagram color={"primary"} onClick={() => { window.open("https://instagram.com/horsesmouth.in?utm_medium=copy_link") }} style={{ cursor: "pointer" }} />
+                    <LinkedIn color={"primary"} onClick={() => { window.open("https://www.linkedin.com/company/horse-s-mouth") }} style={{ cursor: "pointer" }} />
+                    <Twitter color={"primary"} />
                 </Box>
-            </div>
-            <div>
-            <p>UNDERSTAND THE NEXT 2-4 YEARS,IN THE NEXT 20 MINUTES.</p>
-            <p>Email us at
-            raunak.horsesmouth@gmail.com</p>
+            </Box>
+            <div className={styles.otherInfo}>
+                <p>UNDERSTAND THE NEXT 2-4 YEARS, <br/> IN THE NEXT 20 MINUTES.</p>
+                <p className={styles.email} >
+                    Email us at <br/>
+                    raunak.horsesmouth@gmail.com</p>
             </div>
         </Box>
     );
